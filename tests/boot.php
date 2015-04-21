5576246a4af6191a97239e0a2d18f79afb31ba24
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2015-04-22 00:16:59 +0200

    Simplified tests boot file, require patchwork.php

--- tests/boot.php
+++ tests/boot.php
@@ -7,18 +7,19 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-$autoload_path = dirname(dirname(__FILE__)).'/vendor/autoload.php';
-if (! file_exists($autoload_path)) {
-    die('Please install via composer before running tests.');
-}
-require_once $autoload_path;
 
-$helpers_path = dirname($autoload_path).'/phpunit/phpunit/src/Framework/Assert/Functions.php';
-if (! file_exists($helpers_path)) {
-    die('Please install via composer with dev option before running tests.');
+$vendor = dirname(dirname(__FILE__)).'/vendor/';
+
+if (! realpath($vendor)) {
+    die('Please install via Composer before running tests.');
 }
-require_once $helpers_path;
+
+require_once $vendor.'antecedent/patchwork/Patchwork.php';
+require_once $vendor.'autoload.php';
+require_once $vendor.'phpunit/phpunit/src/Framework/Assert/Functions.php';
 
 if (! defined('FOILTESTSBASEPATH')) {
     define('FOILTESTSBASEPATH', __DIR__);
 }
+
+unset($vendor);
