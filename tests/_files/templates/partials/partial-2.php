23b1891273137b3875ac9bdaab7747e283e1940d
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2015-07-06 03:04:17 +0200

    Fix tests mess, remove tests

--- tests/_files/templates/partials/partial-2.php
+++
@@ -1,6 +0,0 @@
-<p>
-    I am another partial.
-    This: <?= $this->v('a_partial_var') ?> is a specific partial var.
-    This: &quot;<?= $this->test_me ?>&quot; is a var I share with template.
-    A shared var skipped: &quot;<?= $this->v('title') ?>&quot;.
-</p>
